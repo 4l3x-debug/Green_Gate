7871eabb5a84a273b4b3ae26dd8da818473cac77
Letícia Lika <lika.ita@hotmail.com>
2021-08-13 10:45:57 -0300

    Atualização - Lojas

---
+++ CSS/style-lojas.php
@@ -0,0 +1,18 @@
+.side_carrosel{
+	width: 250px;
+	height: 150px;
+	background-color: #000;
+	border-radius: 10px;
+	box-shadow: 2px 10px 12px rgba(0,0,0,0.5);
+	position: relative;
+	left: 30px;
+}
+.box_lojas{
+	width: 250px;
+	height: 150px;
+	background-color: #000;
+	border-radius: 10px;
+	box-shadow: 2px 10px 12px rgba(0,0,0,0.5);
+	position: relative;
+	left: 30px;
+}
