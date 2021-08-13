cec6bd623384db93e47812fc12e10206555a42f3
Letícia Lika <lika.ita@hotmail.com>
2021-08-13 09:28:39 -0300

    Atualização - 13/08

---
+++ PHP/lojas.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Green Gate | Lojas</title>
+	<link rel="stylesheet" href="../CSS/style-index.css">
+    <link rel="stylesheet" href="../FONTAW/css/all.css">
+    <link rel="stylesheet" type="text/css" href="../CSS/style_lojas.css">
+    <link rel="shortcut icon" href="../IMG/icone.ico" type="image/x-icon">
+</head>
+<body>
+        <!-- Cabeçalho -->
+
+        <section class="main-nav">
+            <nav>
+                <div class="logo">
+                    <figure>
+                        <a href="../HTML/index
+                        .html"><img src="../IMG/logotipo.png" alt="Logotipo"></a>
+                    </figure>
+                </div>
+
+                <div class="lista-menu">
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Loja</a></li>
+                        <li><a href="">Sobre</a></li>
+                        <li><a href="">Suporte</a></li>
+                    </ul>
+                </div>
+
+                <div class="figuras">
+                    <a href=""><i class="fas fa-search"></i></a>
+                    <a href="../PHP/login.php"><i class="fas fa-user-circle"></i></a>
+                    <a href=""><i class="fas fa-shopping-bag"></i></a>
+                </div>
+            </nav>
+        </section>
+    </header>
+
+
+    <!-- Lojas -->
+
+    <script src="../JS/script_lojas.js" type="text/javascripts"></script>
+
+
+    <section class="slide_carrosel">
+    	<div class="box_lojas">
+    		
+    	</div>
+    </section>
+
+    <!-- Rodapé -->
+
+    <footer class="main-footer">
+        <section class="cont-footer">
+            <div>
+                <p>Para quem se compromete com o meio ambiente.</p>
+            </div>
+        </section>
+
+        <div id="linha-vert"></div>
+
+        <div class="footer-icon">
+            <a href="https://www.facebook.com/Green-Gate-103711395206238"><i class="fab fa-facebook"></i></a>
+            <a href="https://www.instagram.com/green.gate_/"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fas fa-envelope"></i></a>
+        </div>
+
+        <div class="direitos">
+            <p>© Green Gate 2021</p>
+        </div>
+</body>
+</html>
