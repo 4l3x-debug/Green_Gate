2d24fa5da106478d12067176c26c745c039359bf
Letícia Lika <lika.ita@hotmail.com>
2021-09-13 18:52:27 -0300

    Atualização - Página do Administrador

---
+++ PHP/pagina-adm.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Green Gate | Página Administrador</title>
+        <link rel="stylesheet" href="../CSS/index.css">
+        <link rel="stylesheet" type="text/css" href="../CSS/style-pagina-adm.css">
+    	<link rel="stylesheet" href="../FONTAW/css/all.css">
+    	<link rel="shortcut icon" href="../IMG/icone.ico" type="image/x-icon">
+	</head>
+	<body class="corpo-pagina-adm">
+		        <!-- Cabeçalho -->
+
+        <section class="main-nav">
+            <nav>
+                <div class="logo">
+                    <figure>
+                        <a href="../HTML/index.html"><img src="../IMG/logotipo.png" alt="Logotipo"></a>
+                    </figure>
+                </div>
+
+                <div class="figuras">
+                	<a href=""><i class="fas fa-user-circle"></i></a>
+                    <a href=""><i class="far fa-bell"></i></a>
+                </div>
+            </nav>
+        </section>
+    </header>
+
+    <!-- Menu para Administrar -->
+
+    <main>
+    <aside class="main aside">
+    	<nav>
+    		<ul class="icon-aside">
+                <strong>Categorias</strong>
+                <li><a href="#"><i class="fas fa-store-alt"></i>
+                    Lojas
+                </a></li>
+                <li><a href="#"><i class="far fa-check-square"></i> 
+                    Aprovações</a></li>   
+                <li><a href="#"><i class="fas fa-tasks"></i>
+                    Avaliações
+                </a></li>
+                <li><a href="#"><i class="fas fa-headset"></i>
+                    Suporte
+                </a></li>      
+            </ul>
+    	</nav>
+    </aside>
+
+    <!-- Conteúdo -->
+        <section class="main articles">
+            <article class="article um">
+            </article>
+        </section>
+    </main>    
+
+    <!-- Rodapé -->
+
+    <footer class="main-footer">
+        <section class="cont-footer">
+            <div>
+                <p>Para quem se compromete com o meio ambiente.</p>
+            </div>
+        </section>
+
+        <div id="linha-vert"></div>
+
+        <div class="footer-icon">
+            <a href="https://www.facebook.com/Green-Gate-103711395206238"><i class="fab fa-facebook"></i></a>
+            <a href="https://www.instagram.com/green.gate_/"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fas fa-envelope"></i></a>
+        </div>
+
+        <div class="direitos">
+            <p>© Green Gate 2021</p>
+        </div>
+    </footer>
+
+	</body>
+</html>
