184491d2b29f576dcf4c1a39698be1109f43ce9f
Letícia Lika <lika.ita@hotmail.com>
2021-09-25 19:45:18 -0300

    Atualização - Página Lojas

--- PHP/lojas.php
+++ PHP/lojas.php
@@ -4,8 +4,8 @@
 	<meta charset="utf-8">
 	<title>Green Gate | Lojas</title>
 	<link rel="stylesheet" href="../CSS/index.css">
-    <link rel="stylesheet" href="../FONTAW/css/all.css">
     <link rel="stylesheet" type="text/css" href="../CSS/style-lojas.css">
+    <link rel="stylesheet" href="../FONTAW/css/all.css">
     <link rel="shortcut icon" href="../IMG/icone.ico" type="image/x-icon">
 </head>
 <body>
@@ -37,19 +37,188 @@
         </section>
     </header>
 
+    <!-- Banner -->
+    <figure class="banner">    
+        <img src="../IMG/banner.png" align="banner">
+    </figure>
 
-    <!-- Lojas -->
+    <!-- Seção de Lojas -->
 
-    <script src="../JS/script_lojas.js" type="text/javascripts"></script>
-
-
-    <section class="slide_carrosel">
-		<div class="box_lojas one">
-		    <figure>
-		        <img class="lojas" src="#" alt="">
-		    </figure>
-		</div>
+    <section class="main-lojas">
+        <div class="espacamento-lojas">
+            <a href="meu_eco_bebe.php"><article class="lojas">  
+                <img src="../IMG/loja_1.png" alt="meu_eco_bebe"> 
+            </article></a>
+        </div>    
+        <div class="espacamento-lojas">
+            <a href="insecta.php"><article class="lojas">
+                <img src="../IMG/loja_2.png" alt="insecta">
+            </article></a>
+        </div>
+        <div class="espacamento-lojas">
+            <a href="babu.php"><article class="lojas">
+                <img src="../IMG/loja_3.png" alt="babu">
+            </article></a>
+        </div>
+        <div class="espacamento-lojas">
+            <a href="campo_largo.php"><article class="lojas"> 
+                <img src="../IMG/loja_4.png" alt="campo_largo"> 
+            </article></a>
+        </div>
+        <div class="espacamento-lojas">
+            <a href="urban_flowers.php"><article class="lojas">
+                <img src="../IMG/loja_5.png" alt="urban_flowers">
+            </article></a>
+        </div>
     </section>
+
+    <!-- Categorias Lojas -->
+
+    <section class="secao-categorias">
+        <div class="categorias">
+            <div class="espacamento-categoria">
+                <a href="#">
+                    <i class="fas fa-tshirt"></i>
+                </a>
+            </div>
+            <div class="espacamento-categoria">    
+                <a href="#">
+                    <i class="fas fa-air-freshener"></i>
+                </a>
+            </div>
+            <div class="espacamento-categoria">
+                <a href="#">
+                    <i class="fas fa-mug-hot"></i>
+                </a>
+            </div>    
+            <div class="espacamento-categoria">
+                <a href="#">
+                    <i class="fas fa-leaf"></i>
+                </a>
+            </div>
+            <div class="espacamento-categoria">
+                <a href="#">
+                    <i class="fas fa-soap"></i>
+                </a>
+            </div>
+            <div class="espacamento-categoria">
+                <a href="#">
+                    <i class="fas fa-home"></i>
+                </a>
+            </div>
+        </div>
+    </section>
+
+    <!-- Seção Produtos -->
+
+    <section class="secao-produtos">
+        <h2>Produtos mais pesquisados</h2>
+
+        <div class="tamanho-produtos">
+            <div class="espacamento-produtos">
+                <a href="#"><article class="produtos">
+                    <img src="../IMG/produto_1.png" alt="escova_de_dente">
+                </article></a>
+            </div>
+            <div class="espacamento-produtos">
+                <a href="#"><article class="produtos">
+                    <img src="../IMG/produto_2.png" alt="sabonete">
+                </article></a>
+            </div>
+            <div class="espacamento-produtos">
+                <a href="#"><article class="produtos">
+                    <img src="../IMG/produto_3.png" alt="eco_bag">
+                </article></a>
+            </div>
+        </div>
+
+        <section class="produtos-casa">
+            <h2>Para sua casa</h2>
+            <div class="container-casa">
+                    <div class="info um">  
+                        <img src="../IMG/prod_canudo.jpg" alt="produto1">  
+                        <span class="preco"> R$ 29,99 </span>  
+                        <p><a href="" class="descricao"> Kit 4 Canudos Inox + Saco Reciclável</a></p>
+                    </div>
+                    <div class="info">  
+                        <img src="../IMG/prod_ecobag.png" alt="produto2">  
+                        <span class="preco"> R$ 35,00 </span>
+                        <p><a href="" class="descricao"> 1 Sacola Sustetável</a></p>
+                    </div>
+                    <div class="info">
+                        <img src="../IMG/prod_escova.jpg" alt="produto3">
+                        <span class="preco"> R$ 15,00 </span>
+                        <p><a href="" class="descricao"> Kit 5 Escovas de Bambu</a></p>
+                    </div>
+                    <div class="info">
+                        <img src="../IMG/prod_copo.jpg" alt="produto4">
+                        <span class="preco"> R$ 14,99 </span>
+                        <p><a href="" class="descricao"> Kit 10 Copos Recicláveis</a></p>
+                    </div>  
+            </div>
+
+        </section>
+
+        <section class="produtos-recentes">
+            <h2>Produtos Colocados Recentemente</h2>
+            <div class="container-recente">
+                    <div class="info um">  
+                        <img src="../IMG/prod_camiseta.jpg" alt="camiseta">  
+                        <span class="preco"> R$ 19,00 </span>  
+                        <p><a href="" class="descricao"> Camiseta 100% Algodão</a></p>
+                    </div>
+                    <div class="info">  
+                        <img src="../IMG/prod_copo-festa.png" alt="copos">  
+                        <span class="preco"> R$ 2,50 </span>
+                        <p><a href="" class="descricao"> 1 Copa | Reciclável</a></p>
+                    </div>
+                    <div class="info">
+                        <img src="../IMG/prod_kitbemglo.png" alt="kit_bemglo">
+                        <span class="preco"> R$ 39,99 </span>
+                        <p><a href="" class="descricao"> Kit Bemglô</a></p>
+                    </div>
+                    <div class="info">
+                        <img src="../IMG/prod_camiseta2.jpg" alt="camisa">
+                        <span class="preco"> R$ 10,00 </span>
+                        <p><a href="" class="descricao"> Camiseta Branca | Algodão</a></p>
+                    </div>  
+            </div>
+
+        </section>
+
+        <section class="sessao-propaganda">
+            <img class="propaganda" src="../IMG/Bee-Green_fundo.gif" alt="">
+        </section>
+
+        <section class="sessao-estilo">
+            <h2>Produtos Estilosos</h2>
+            <div class="container-estilo">
+                    <div class="info um">  
+                        <img src="../IMG/prod_camiseta.jpg" alt="camiseta">  
+                        <span class="preco"> R$ 19,00 </span>  
+                        <p><a href="" class="descricao"> Camiseta 100% Algodão</a></p>
+                    </div>
+                    <div class="info">  
+                        <img src="../IMG/prod_copo-festa.png" alt="copos">  
+                        <span class="preco"> R$ 34,99 </span>
+                        <p><a href="" class="descricao"> Camiseta Azul | Reciclável </a></p>
+                    </div>
+                    <div class="info">
+                        <img src="../IMG/prod_kitbemglo.png" alt="kit_bemglo">
+                        <span class="preco"> R$ 49,00 </span>
+                        <p><a href="" class="descricao"> Camiseta Colcci </a></p>
+                    </div>
+                    <div class="info">
+                        <img src="../IMG/prod_camiseta2.jpg" alt="camisa">
+                        <span class="preco"> R$ 50,00 </span>
+                        <p><a href="" class="descricao"> Camiseta Arroz | Algodão</a></p>
+                    </div>  
+            </div>
+
+        </section>
+
+    </section>
+
 
     <!-- Rodapé -->
 
