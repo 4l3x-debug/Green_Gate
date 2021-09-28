46e043cf158aaacbf6c49f3752f70772b89caae8
4l3x-debug <alex.silvacarvalho015@gmail.com>
2021-09-28 19:08:34 -0300

    Lojas - Alex

--- PHP/lojas.php
+++ PHP/lojas.php
@@ -1,38 +1,5 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head>
-        <title>Lojas</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="../CSS/loja.css">
-        <link rel="stylesheet" href="../FONTAW/css/all.css">
-        <link rel="shortcut icon" href="../IMG/icone.ico" type="image/x-icon">
-    </head>
-    <body>
-        <header class="main-nav">
-                <nav>
-                    <div class="logo">
-                        <figure>
-                            <a href="index
-                            .html"><img src="../IMG/LOJAS/logotipo.png" alt="Logotipo"></a>
-                        </figure>
-                    </div>
-    
-                    <div class="lista-menu">
-                        <ul>
-                            <li><a href="index.php">Home</a></li>
-                            <li><a href="lojas.php">Loja</a></li>
-                            <li><a href="sobre.php">Sobre</a></li>
-                            <li><a href="suporte.php">Suporte</a></li>
-                        </ul>
-                    </div>
-    
-                    <div class="figuras">
-                        <a href=""><i class="fas fa-search"></i></a>
-                        <a href="login.php"><i class="fas fa-user-circle"></i></a>
-                        <a href=""><i class="fas fa-shopping-bag"></i></a>
-                    </div>
-                </nav>
-        </header>
 <head>
 	<meta charset="utf-8">
 	<title>Green Gate | Lojas</title>
@@ -44,25 +11,30 @@
 <body>
         <!-- Cabeçalho -->
 
-        <section>
-            <div class="container-site">
-                <img class="first-image" src="../IMG/LOJAS/compre-agora-1.png">
-            </div>
-        </section>
-<<<<<<< HEAD
+        <section class="main-nav">
+            <nav>
+                <div class="logo">
+                    <figure>
+                        <a href="index.php"><img src="../IMG/logotipo.png" alt="Logotipo"></a>
+                    </figure>
+                </div>
 
-        <section class="lojas">
-        <div class="container-loja">
-            <a href=""> <img src="../IMG/LOJAS/loja1.jpg" alt="Loja1"> </a>
-            <a href=""> <img src="../IMG/LOJAS/loja2.jpg" alt="Loja2"> </a>
-            <a href=""> <img src="../IMG/LOJAS/loja3.jpg" alt="Loja3"> </a>
-            <a href=""> <img src="../IMG/LOJAS/loja4.jpg" alt="Loja4"> </a>
-            <a href=""> <img src="../IMG/LOJAS/loja5.jpg" alt="Loja4"> </a>
-            <a href=""> <img src="../IMG/LOJAS/loja6.png" alt="Loja4"> </a>
-        </div>
-        </section>
+                <div class="lista-menu">
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="lojas.php">Loja</a></li>
+                        <li><a href="sobre.php">Sobre</a></li>
+                        <li><a href="suporte.php">Suporte</a></li>
+                    </ul>
+                </div>
 
-        <section class="principais-produtos">
+                <div class="figuras">
+                    <a href=""><i class="fas fa-search"></i></a>
+                    <a href="login.php"><i class="fas fa-user-circle"></i></a>
+                    <a href=""><i class="fas fa-shopping-bag"></i></a>
+                </div>
+            </nav>
+        </section>
     </header>
 
     <!-- Banner -->
@@ -249,62 +221,25 @@
 
 
     <!-- Rodapé -->
-            <span> Categorias mais pesquisadas </span>
-            <a href="#" class="mais-produtos"> Ver todos os Produtos </a>
-            
-            <div class="produto-container">
-                <a href="mais-procurados1.php"> <img src="../IMG/LOJAS/canudo.jpg" alt="canudo" class="canudo"> </a>
-                <a href="principais-produtos-2.php"> <img src="../IMG/LOJAS/ecobag.jpeg" alt="ecobag"> </a>
-            </div> 
-        </section> 
-        
-        <section class="sessao-casa">
-            <div class="fundo">
-            <span class="para-casa"> Para sua casa </span>
-            <div class="container-casa">
-                    <div class="info1">  <img src="../IMG/LOJAS/prod_canudo.jpg" alt="produto1">  <span class="preco"> R$ 29,99 </span> <br> <a href="" class="descricao"> Kit 4 Canudos Inox + Saco Reciclável</a> </div>
-                    <div class="info2">  <img src="../IMG/LOJAS/prod_ecobag.png" alt="produto2">  <span class="preco"> R$ 35,00 </span> <br> <a href="" class="descricao"> 1 Sacola Sustetável </a> </div>
-                    <div class="info3">  <img src="../IMG/LOJAS/prod_escova.jpg" alt="produto3">  <span class="preco"> R$ 15,00 </span> <br> <a href="" class="descricao"> Kit 5 Escovas de Bambu </a> </div>
-                    <div class="info4">  <img src="../IMG/LOJAS/prod_copo.jpg" alt="produto4">  <span class="preco"> R$ 14,99 </span> <br> <a href="" class="descricao"> Kit 10 Copos Recicláveis </a> </div>
-                    <div class="espace">  </div>    
+
+    <footer class="main-footer">
+        <section class="cont-footer">
+            <div>
+                <p>Para quem se compromete com o meio ambiente.</p>
             </div>
         </section>
 
-        <section class="sessao-recente">
-            <span class="prod-recente"> Produtos Colocados Recentemente </span>
-            <div class="container-recente">
-            <div class="info5"> <img src="../IMG/LOJAS/prod_camiseta.jpg" alt="camiseta">  <span class="preco"> R$ 19,00 </span> <br> <a href="" class="descricao"> Camiseta 100% Algodão </a> </div>
-            <div class="info6"> <img src="../IMG/LOJAS/prod_copo-festa.png" alt="copos">  <span class="preco"> R$ 2,50 </span> <br> <a href="" class="descricao"> 1 Copo | Reciclável </a> </div>
-            <div class="info7"> <img src="../IMG/LOJAS/prod_kitbemglo.png" alt="kitbemglo">  <span class="preco"> R$ 39,99 </span> <br> <a href="" class="descricao"> Kit Bemglô </a> </div>
-            <div class="info8"> <img src="../IMG/LOJAS/prod_camiseta2.jpg" alt="camiseta">  <span class="preco"> R$ 10,00 </span> <br> <a href="" class="descricao"> Camiseta Branca | Algodão </a> </div>
-            </div>    
-        </section>
-        
-        <section class="sessao-propaganda">
-            <div> <img class="propaganda" src="../IMG/LOJAS/Bee-Green_fundo.gif" alt=""> </div>
-            <a href=""><img class="Loja-Propaganda" src="../IMG/LOJAS/Loja6.png" alt="Loja"></a>
-        </section>
+        <div id="linha-vert"></div>
 
-        <section class="sessao-estilo">
-            <span class="fique-no-estilo"> Fique no Estilo </span>
-            <div class="container-recente">
-            <div class="info5"> <img src="../IMG/LOJAS/prod_camiseta.jpg" alt="camiseta">  <span class="preco"> R$ 30,00 </span> <br> <a href="" class="descricao"> Camiseta 100% Algodão </a> </div>
-            <div class="info6"> <img src="../IMG/LOJAS/camiseta-azul.jpg" alt="camiseta">  <span class="preco"> R$ 34,99 </span> <br> <a href="" class="descricao"> Camiseta Azul | Reciclável </a> </div>
-            <div class="info7"> <img src="../IMG/LOJAS/camiseta-colcci.jpg" alt="camiseta">  <span class="preco"> R$ 49,00 </span> <br> <a href="" class="descricao"> Camiseta Colcci </a> </div>
-            <div class="info8"> <img src="../IMG/LOJAS/camiseta-arroz.jpg" alt="camiseta">  <span class="preco"> R$ 50,00 </span> <br> <a href="" class="descricao"> Camiseta Arroz | Algodão </a> </div>
-            </div>    
-        </section>
+        <div class="footer-icon">
+            <a href="https://www.facebook.com/Green-Gate-103711395206238"><i class="fab fa-facebook"></i></a>
+            <a href="https://www.instagram.com/green.gate_/"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fas fa-envelope"></i></a>
+        </div>
 
-        <section class="sessao-categoria"> 
-            <a href=""> <div class="categoria"> <img src="../IMG/LOJAS/t-shirt-icon.svg" alt=""> </div> </a>
-            <a href=""> <div class="categoria"> <img src="../IMG/LOJAS/cup-icon.svg" alt=""> </div> </a>
-            <a href=""> <div class="categoria"> <img src="../IMG/LOJAS/favorite-icon.svg" alt=""> </div> </a>
-            <a href=""> <div class="categoria"> <img src="../IMG/LOJAS/escova-icon.png" alt=""> </div> </a>
-            <a href=""> <div class="categoria"> <img src="../IMG/LOJAS/cosmetics-icon.png" alt=""> </div> </a>
-            <a href=""> <div class="categoria"> <img src="../IMG/LOJAS/promocao-icon.png" alt=""> </div> </a>
-            <a href=""> <div class="categoria"> <img src="../IMG/LOJAS/ecobag-icon.png" alt=""> </div> </a>
-            <a href=""> <div class="categoria"> <img src="../IMG/LOJAS/casa-icon.png" alt=""> </div> </a>
-        </section>
+        <div class="direitos">
+            <p>© Green Gate 2021</p>
+        </div>
 
-        </body>
+</body>
 </html>
