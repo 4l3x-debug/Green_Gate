cacbc2ad452d7fdd4ba5d6942d12fb958ab5ac6a
Letícia Lika <lika.ita@hotmail.com>
2021-10-19 10:22:38 -0300

    Atualização - 19/10

--- PHP/notificacoes_produtor.php
+++ PHP/notificacoes_produtor.php
@@ -5,7 +5,7 @@
         <title>Green Gate | Notificações</title>
         <link rel="stylesheet" href="../CSS/style-index.css">
         <link rel="stylesheet" type="text/css" href="../CSS/style-painel-produtor.css">
-        <link rel="stylesheet" type="text/css" href="../CSS/pagina_usuario.css">
+        <link rel="stylesheet" type="text/css" href="../CSS/style-pagina_usuario.css">
         <link rel="stylesheet" href="../FONTAW/css/all.css">
         <link rel="shortcut icon" href="../IMG/icone.ico" type="image/x-icon">
     </head>
