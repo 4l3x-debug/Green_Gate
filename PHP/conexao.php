0463c24d97590972a354e92667c162732f6b1367
4l3x-debug <alex.silvacarvalho015@gmail.com>
2021-10-25 12:21:28 -0300

    Ajustes finais - Index - Alex

--- PHP/conexao.php
+++ PHP/conexao.php
@@ -1,3 +1,3 @@
 <?php
-    $conectar = mysqli_connect('localhost', 'root', '', 'green_gate') or die("Erro ao conectar-se com o banco" . mysqli_error(''));
+    $conectar = mysqli_connect('localhost', 'root', '1020', 'green_gate') or die("Erro ao conectar-se com o banco" . mysqli_error(''));
 ?>
