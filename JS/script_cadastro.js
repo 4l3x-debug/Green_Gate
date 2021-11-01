b8422c532e695774b2da574ba0b49e77ee2af78e
Letícia Lika <lika.ita@hotmail.com>
2021-11-01 13:11:15 -0300

    Atualização

---
+++ JS/script_cadastro.js
@@ -0,0 +1,9 @@
+function containerDisplay(){
+	document.getElementById("primeira").style.display="none";
+	document.getElementById("segunda").style.display="block";
+}
+
+function Voltar(){
+	document.getElementById("primeira").style.display="block";
+	document.getElementById("segunda").style.display="none";
+}
