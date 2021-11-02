bc7160800ecf0ef1a85ae623eb57954457b832e9
4l3x-debug <alex.silvacarvalho015@gmail.com>
2021-11-02 15:07:37 -0300

    cadastro_pf_fisica

--- PHP/conexao.php
+++ PHP/conexao.php
@@ -1,9 +1,3 @@
 <?php
-<<<<<<< HEAD
     $conectar = mysqli_connect('localhost', 'root', '1020', 'green_gate_final') or die("Erro ao conectar-se com o banco" . mysqli_error(''));
-=======
-
-    $conectar = mysqli_connect('localhost', 'root', '1020', 'green_gate_final') or die("Erro ao conectar-se com o banco" . mysqli_error(''));
-
->>>>>>> 571766243c43800ae809d0e2a38ab46df5bd7ebc
 ?>
