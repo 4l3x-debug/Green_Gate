f481df918658ac34098e4a69f3e42d0f924c1cac
Letícia Lika <lika.ita@hotmail.com>
2021-11-05 19:57:49 -0300

    Atualização - 05/11

--- PHP/Produtor/endereco.php
+++ PHP/Produtor/endereco.php
@@ -48,7 +48,7 @@
                 </div>
 
                 <div class="figuras-produtor">
-                    <a href="pagina_usuario_produtor.php"><i class="fas fa-user-circle"></i>
+                    <a href="painel_produtor.php"><i class="fas fa-user-circle"></i>
                         <div class="usuario">
                             <?php echo $dados_usuario['nome']; ?>        
                         </div>
