47a42565aeaf6d7b8b69e6ed36ff0d1d4c04f7c7
Letícia Lika <lika.ita@hotmail.com>
2021-11-06 21:46:26 -0300

    Atualização - 06/11

--- PHP/Produtor/suporte.php
+++ PHP/Produtor/suporte.php
@@ -71,7 +71,7 @@
 
         <aside id="menuOculto" class="menuOculto">
             <a href="javascript: void(0)" class="btnFechar" onclick="fecharNav()"><i class="fas fa-times"></i></a>
-            <a href="produtos.php" class="icon"><i class="fas fa-store-alt"></i>Produtos</a>
+            <a href="produtos.php?edit=0" class="icon"><i class="fas fa-store-alt"></i>Produtos</a>
             <a href="#" class="icon"><i class="fas fa-headset"></i>Suporte</a>
             <a href="../invalido.php" class="icon"><i class="fas fa-sign-out-alt"></i>Sair</a>
         </aside>
