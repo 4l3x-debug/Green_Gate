cde47f498883b9a3b8dd9fae05f4ef0f6db31923
Letícia Lika <lika.ita@hotmail.com>
2021-11-16 21:52:35 -0300

    Merge branch 'main' of https://github.com/4l3x-debug/Green_Gate

---
+++ JS/script_editar_perfil.js
@@ -0,0 +1,8 @@
+function foto(){
+	let photo = document.getElementById('tamanho');
+	let file = document.getElementById('imagem');
+
+	photo.addEventListener('click', () => {
+	    file. click();
+	});
+}
