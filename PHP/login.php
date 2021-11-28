66cc29923706e895dd73756fe56fd6b4e814aeb7
Letícia Lika <lika.ita@hotmail.com>
2021-11-28 19:44:39 -0300

    Atualização - 28/11

--- PHP/login.php
+++ PHP/login.php
@@ -10,13 +10,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+      <link rel="stylesheet" type="text/css" href="../CSS/style-login.css">
       <link rel="shortcut icon" href="../IMG/icone.ico" type="image/x-icon">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-    <link rel="stylesheet" type="text/css" href="../CSS/style-login.css">
-
   <body>
 
     <div class="container-fluid position-absolute"> <!-- INÍCIO - Tamanho da página-->
